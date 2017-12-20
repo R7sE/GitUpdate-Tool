1191488bc29c4fac314c1418bb757d20eaeb50e9
JcluoIvan <jcluo.ivan@gmail.com>
2017-12-20 17:02:18 +0800

    update

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const Console = require('./src/console');
 // let cmd = new GitCommand('D:/work/GitHub/LatticeApp');
 
 // let cmd = new GitCommand('D:/work/GitHub/javaserver44');
-let cmd = new GitCommand('D:/work/GitHub/sg44_w');
-// let cmd = new GitCommand('D:/work/GitHub/sg44_c');
+let cmd = new GitCommand('D:/work/GitHub/sg44_c');
+// let cmd = new GitCommand('D:/work/GitHub/sg44_w');
 // let cmd = new GitCommand('D:/work/GitHub/sg44_a');
 
 ([
-    0, 1, 3, 2, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 21, 22, 24,
-    25, 26, 27, 28, 29,
-    30, 32, 34, 35, 36, 37, 40, 41, 42, 43, 45, 46, 51,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 81, 82,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 24, 25, 26, 27, 28, 29, 30, 32, 34, 36, 37, 39, 40, 41, 42, 43, 45, 46, 51
+
 ])
 .forEach(sg => {
     Console.info(`開始 sg${sg} 流程`);
@@ -19,16 +19,15 @@ let cmd = new GitCommand('D:/work/GitHub/sg44_w');
         .push()
         // .reset(`origin/sg${sg}`)
         // .merge('master-自訂規則改成開放柱碰、連柱碰')
+        // .merge('master-new-service-no-outbet')
+        // .merge('master-new-service')
         // .merge('master')
-        // .merge('issue55-sg44_c')
-        // .merge('master-star234規則-2版');
-        // .merge('master-自訂義234星規則2版');
-        // .merge('master-自訂義234星規則2版-連柱碰Only');
-        // .merge('master-自訂義234星規則-連柱碰only');
-        // .merge('master-自訂義234星規則-2版(柱碰and連柱碰)');
+        // .merge('newsky23_six')
+        // .merge('blockSky')
         ;
     return;
-    let hasMerge = false;
+    // return;
+    // let hasMerge = false;
     // cmd.unmergeAddeds().forEach(fname => {
     //     cmd.added(fname);
     //     Console.warning(`衝突檔案: ${fname}`);
@@ -41,10 +40,10 @@ let cmd = new GitCommand('D:/work/GitHub/sg44_w');
     //     hasMerge = true;
     // });
 
-    if (hasMerge) {
-        Console.info(`Commit sg${sg}`);
-        cmd.commit('auto merge');
-    }
+    // if (hasMerge) {
+    //     Console.info(`Commit sg${sg}`);
+    //     cmd.commit('auto merge');
+    // }
     Console.success('合併完成');
     Console.success('==============================');
     exportDiff(sg);
@@ -68,9 +67,9 @@ function exportDiff (sg) {
         newSHA,
         exceStatus: ['D'],
         exceFile: ['^conf', '^.git', '^\.conf', 'config\.php'],
-        outpath: 'D:/work/UpdateQueue/20170727-1030',
-        dirname: `javaserver${sg}`,
-        // dirname: `sg${sg}_c`,
+        outpath: 'D:/work/UpdateQueue/2017107-1020',
+        // dirname: `javaserver${sg}`,
+        dirname: `sg${sg}_c`,
         // dirname: `sg${sg}_w`,
     });
 
